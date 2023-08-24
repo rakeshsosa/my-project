e5efc8ac34817e93b40d8881f21f732bd1f78305
rakeshsosa <rakeshsosa.arete@tranzcom.co.in>
2023-08-24 16:19:02 +0530

    KGA-11 resolved

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -10,8 +10,8 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("my name is rakesh");
         System.out.println("Hello world!");
+        System.out.println("this is second statement...");
     }
     
 }
