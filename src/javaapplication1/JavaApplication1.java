50ba7c92956059c2195aebdc17a148f7fb463f9c
rakeshsosa <rakeshsosa.arete@tranzcom.co.in>
2023-08-24 10:51:27 +0530

    Adding second commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -11,6 +11,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello new world!");
+        System.out.println("Hello world!");
     }
     
 }
